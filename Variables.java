adc5fb9ffab6014cc2b98fb8675270d7f168ce9b
Britt Carter <brittbmh@gmail.com>
2019-04-04 09:35:49 -0500

    add variables

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        // primitive variables
+        // whole numbers, negative or positive
+        int num1 = 15;
+        // numbers with decimals (with limit)
+        double num2 = 85234.25;
+        // true or false
+        boolean javaIsNewToMe = true;
+        // single letter or punctuation (use single quotes)
+        char firstInitial = 'B';
+        // Non-primitive variables
+        // Strings are Objects (use double quotes)
+        String name = "Britt";
+
+    }
+}
