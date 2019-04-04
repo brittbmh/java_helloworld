0087eeb68c264d4036bbc1bd42fa11cc278a288d
Britt Carter <brittbmh@gmail.com>
2019-04-04 09:45:35 -0500

    add Maths

---
+++ Maths.java
@@ -0,0 +1,8 @@
+public class Maths {
+    public static void main(String[] args){
+        double num1 = 3544.24765;
+        double num2 = 35741.5284;
+        double sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
