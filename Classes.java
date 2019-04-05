16d7c5f42d8fc99d166cc6ebeeb02f7fbca3048f
Britt Carter <brittbmh@gmail.com>
2019-04-05 16:20:42 +0200

    add Store constructor and instance

--- Classes.java
+++ Classes.java
@@ -1,8 +1,9 @@
 public class Store {
     // declare instance fields here!
-
+    String productType;
     // constructor method
-    public Store() {
+    public Store(String product) {
+        productType = product;
         System.out.println("I am inside the constructor method.");
     }
 
@@ -10,8 +11,7 @@ public class Store {
     public static void main(String[] args) {
         System.out.println("This code is inside the main method.");
 
-        Store lemonadeStand = new Store();
-
-        System.out.println(lemonadeStand);
+        Store lemonadeStand = new Store("lemonade");
+        System.out.println(lemonadeStand.productType);
     }
 }
