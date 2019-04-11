381a302731b15ca53be3fe5c266d3f94f0f60055
Britt Carter <brittbmh@gmail.com>
2019-04-11 16:21:56 -0500

    add Booleans and initial examples

---
+++ files/Booleans.java
@@ -0,0 +1,10 @@
+public class Booleans {
+    public static void main(String[] args){
+        // false statements
+        System.out.println(23 >= 14 && 23 >= 25);
+        
+        System.out.println(!(4 <= 10));
+
+        // true statements
+    }
+}
