118039e10ad95a0590c68b29f1db38852a7eb8ec
Britt Carter <brittbmh@gmail.com>
2019-04-11 16:49:03 -0500

    add more examples to Booleans

--- files/Booleans.java
+++ files/Booleans.java
@@ -1,5 +1,5 @@
 public class Booleans {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // false statements
         System.out.println(23 >= 14 && 23 >= 25);
         System.out.println(!(1 < 8 || (5 > 2 && 3 < 5)));
@@ -10,7 +10,7 @@ public class Booleans {
         System.out.println(!(false) || true && false);
 
         // Control Flow
-        if (9> 2){
+        if (9 > 2) {
             System.out.println("Control flow rocks!");
         }
 
@@ -37,5 +37,22 @@ public class Booleans {
 
         char canDrive = (fuelLevel > 0) ? 'Y' : 'N';
         System.out.println(canDrive);
+
+        // switch statement
+        char penaltyKick = 'R';
+
+        switch (penaltyKick) {
+            case 'L':
+                System.out.println("Messi shoots to the left and scores!");
+                break;
+            case 'R':
+                System.out.println("Messi shoots to the right and misses the goal!");
+                break;
+            case 'C':
+                System.out.println("Messi shoots down the center, but the keeper blocks it!");
+                break;
+            default:
+                System.out.println("Messi is in position...");
+        }
     }
 }
